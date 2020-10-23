df3aa32d9583c797dc31b2091fc1d7722c338673
Big-Nurpin <sdean.jeremy@gmail.com>
2020-10-22 17:19:22 -0700

    Tidied up the teest and training data and merged the two

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,22 @@ rawDataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUC
 download.file(rawDataURL, destfile = "rawData.zip")
 unzip("rawData.zip")
 
+##Load the raw data into data frames and add subject ID's and variable labels
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+features <- scan("UCI HAR Dataset/features.txt", what = "character", sep = "\n")
+features <- sub("[0-9]+ ", "", features)
+names(train) <- features
+train$labels <- scan("UCI HAR Dataset/train/Y_train.txt", what = "factor",
+                      sep = "\n")
+train$id <- scan("UCI HAR Dataset/train/subject_train.txt", what = "factor",
+                  sep = "\n")
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+names(test) <- features
+test$labels <- scan("UCI HAR Dataset/test/Y_test.txt", what = "factor",
+                     sep = "\n")
+test$id <- scan("UCI HAR Dataset/test/subject_test.txt", what = "factor",
+                 sep = "\n")
+
+##Merge the training and test data
+mergedData <- rbind(train, test)
